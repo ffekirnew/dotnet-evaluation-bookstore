0539a62eacb23e6cbd7a948e0fe1fa2f7c997111
ffekirnew <ffekirnew0808@gmail.com>
2024-04-01 12:41:12 +0300

    feat: Implement the domain layer

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace Domain.Common;
+
+public class BaseEntity
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime LastModified { get; set; }
+}
---
+++ Domain/Entitites/Book.cs
@@ -0,0 +1,14 @@
+using Domain.Common;
+
+namespace Domain.Entities;
+
+public class BookEntity : BaseEntity
+{
+  public string? Title { get; set; }
+  public string? Author { get; set; }
+  public string? Description { get; set; }
+  public string? Category { get; set; }
+  public string? Language { get; set; }
+  public int TotalPages { get; set; }
+}
+
